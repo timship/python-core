6a8b8d147d7393e48046b359082050c4316fbf26
Mariya Shkurat <57259035+timship@users.noreply.github.com>
2026-02-19 19:42:10 +0300

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,65 @@
+"""
+Задание №1
+Для класса Book реализуйте класс Library, который будет обладать свойствами итератора,
+однако итерирование возможно в библиотеке только по тем книгам,
+которые не находятся в пользовании у читателей (book.is_use = False).
+"""
+class Book:
+    def __init__(self, title, is_use):
+        self.title = title
+        self.is_use = is_use
+
+    def __repr__(self):
+        return f"Book('{self.title}', {self.is_use})"
+
+"""
+class Library:
+
+    def __init__(self, books):
+        self.books = books
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        while self.index < len(self.books):
+            book = self.books[self.index]
+            self.index += 1
+
+            if not book.is_use:
+                return book
+        raise StopIteration
+"""
+
+class Library:
+    def __init__(self, books):
+        self.books = books
+
+    def __iter__(self):
+        self.index = -1
+        return self
+
+    def __next__(self):
+        self.index += 1
+        if self.index >= len(self.books):
+            raise StopIteration
+        if self.books[self.index].is_use:
+            return self.__next__()
+        return self.books[self.index]
+
+
+books = [
+    Book("Python for Beginners", True),
+    Book("Python in Action", False),
+    Book("Hands-On Machine Learning", False),
+    Book("Introduction to Machine Learning with Python", True),
+    Book("The Hundred-page Machine Learning Book", False),
+]
+
+library = Library(books)
+for b in library:
+    print(f"{b.title}")
+# Python in Action
+# Hands-On Machine Learning
+# The Hundred-page Machine Learning Book
---
+++ project2.py
@@ -0,0 +1,34 @@
+"""
+Задание №2
+Реализуйте функцию-генератор generate_values(f, start_x, end_x, step),
+которая будет создавать генератор значений функции f(x) на заданном промежутке [start_x, end_x] с шагом step.
+
+При реализации для точности сравнений чисел выполняйте округления до 2-ого знака после запятой.
+
+Пример:
+"""
+
+def generate_values(f, start_x, end_x, step):
+    x = start_x
+    while round(x, 2) <= end_x:
+        yield f(x)
+        x += step
+
+f1 = lambda x: 2*x + 1
+
+gen1 = generate_values(f1, 0, 1, 0.1)
+values1 = []
+for y in gen1:
+    values1.append(round(y,2))
+print(values1) # result = [1, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0]
+
+gen2 = generate_values(f1, 0, 2, 0.1)
+values2 = []
+for y in gen2:
+    values2.append(round(y,2))
+print(values2) # result = [1, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.2, 4.4, 4.6, 4.8, 5.0]
+
+
+
+
+
